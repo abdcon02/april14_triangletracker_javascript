e7e31309d4c67b31f95b1e93302890abe30bc841
Tanner Heffner <tannerjheffner@gmail.com>
2015-04-10 09:25:42 -0700

    add spec/logic for right triangles

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 var triangle = function(a, b, c) {
 
   var lengths = [Math.abs(a), Math.abs(b), Math.abs(c)],
-      sorted = [];
+      sorted = [],
+      squares = [];
 
 //sorts inputs from smallest to largest
   sorted = lengths.sort(function(a, b) { return a-b});
 
+  squares[0] = Math.pow(sorted[0], 2);
+  squares[1] = Math.pow(sorted[1], 2);
+  squares[2] = Math.pow(sorted[2], 2);
+
+  console.log(squares);
+
+
   if (sorted[0] + sorted[1] > sorted[2]) {
 
-    if ((sorted[0] != sorted[1]) && (sorted[0] != sorted[2]) && (sorted[1] != sorted[2])) {
+    if ((sorted[0] != sorted[1]) && (sorted[0] != sorted[2]) && (sorted[1] != sorted[2])
+        && (squares[0] + squares[1] != squares[2])) {
       return "scalene";
     }
 
@@ -18,6 +27,10 @@ var triangle = function(a, b, c) {
 
     if ((sorted[0] === sorted[1]) && (sorted[0] === sorted[2]) && (sorted[1] === sorted[2])) {
       return "equilateral";
+    }
+
+    if (squares[0] + squares[1] === squares[2]) {
+      return "right";
     }
 
   } else {
