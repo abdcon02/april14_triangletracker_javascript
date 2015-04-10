5f0044b312b644608f201207578f45d9c61c64ef
Tanner Heffner <tannerjheffner@gmail.com>
2015-04-10 09:31:47 -0700

    add one last note

--- js/scripts.js
+++ js/scripts.js
@@ -9,34 +9,34 @@ var triangle = function(a, b, c) {
 //sorts inputs from smallest to largest
   sorted = lengths.sort(function(a, b) { return a-b});
 
-
 //squares sorted values for right triangle check
   for (i = 0; i < sorted.length; i++) {
     squares[i] = Math.pow(sorted[i], 2);
   }
 
+// if it's a triangle
   if (sorted[0] + sorted[1] > sorted[2]) {
 
-//all sides different
-    if ((sorted[0] != sorted[1]) && (sorted[0] != sorted[2]) && (sorted[1] != sorted[2])
-        && (squares[0] + squares[1] != squares[2])) {
-      return "scalene";
-    }
+  //all sides different
+      if ((sorted[0] != sorted[1]) && (sorted[0] != sorted[2]) && (sorted[1] != sorted[2])
+          && (squares[0] + squares[1] != squares[2])) {
+        return "scalene";
+      }
 
-//two sides the same
-    if ((sorted[0] === sorted[1]) && (sorted[0] != sorted[2])) {
-      return "isosceles";
-    }
+  //two sides the same
+      if ((sorted[0] === sorted[1]) && (sorted[0] != sorted[2])) {
+        return "isosceles";
+      }
 
-//all sides the same
-    if ((sorted[0] === sorted[1]) && (sorted[0] === sorted[2]) && (sorted[1] === sorted[2])) {
-      return "equilateral";
-    }
+  //all sides the same
+      if ((sorted[0] === sorted[1]) && (sorted[0] === sorted[2]) && (sorted[1] === sorted[2])) {
+        return "equilateral";
+      }
 
-// a^2 + b^2 = c^2
-    if (squares[0] + squares[1] === squares[2]) {
-      return "right";
-    }
+  // a^2 + b^2 = c^2
+      if (squares[0] + squares[1] === squares[2]) {
+        return "right";
+      }
 
 //not a triangle
   } else {
