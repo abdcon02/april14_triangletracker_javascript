57250313dfa900428388d442e276c43cfbdf5aa6
Tanner Heffner <tannerjheffner@gmail.com>
2015-04-10 08:53:57 -0700

    Add sorting function to make user input order irrelevant

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 var triangle = function(a, b, c) {
-  if (a + b > c) {
+
+  var lengths = [a, b, c],
+      sorted = [];
+
+//sorts inputs from smallest to largest
+  sorted = lengths.sort(function(a, b) { return a-b});
+
+  if (sorted[0] + sorted[1] > sorted[2]) {
     return true;
+  } else {
+    return false;
   }
+
 }
--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,13 @@ describe('triangle', function() {
   it("will say it is a triangle for inputs: (3, 4, 5)", function() {
     expect(triangle(3, 4, 5)).to.equal(true);
   });
+
+  it("will say it is not a triangle for inputs: (4, 5, 10)", function() {
+    expect(triangle(4, 5, 10)).to.equal(false);
+  });
+
+  it("will say it is a triangle for inputs: (3, 4, 5)", function() {
+    expect(triangle(3, 4, 5)).to.equal(true);
+  });
+
 });
